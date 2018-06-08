000f8c1e227714bf95fd165a341641eeb38e8ab8
Roland Züger <rzueger@tocco.ch>
2018-06-08 20:43:05 +0200

    Display nice sunny sky in empty cards

--- src/components/ItemGroup/ItemGroup.js
+++ src/components/ItemGroup/ItemGroup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './ItemGroup.css';
+import emptyImg from './empty.png';
 
 import typeFactory from './typeFactory';
 
@@ -21,12 +22,30 @@ class ItemGroup extends React.Component {
           <div className={`card-header ${s['card-header']}`}>
             {this.props.group.title}
           </div>
-          <div className={`card-body ${s['card-body']}`}>
-            {this.props.group.children
-              .filter(item => !!item)
-              .map(item => typeFactory(item))}
-          </div>
+          <div className={`card-body ${s['card-body']}`}>{this.getBody()}</div>
         </div>
+      </div>
+    );
+  }
+
+  getBody() {
+    const children = this.props.group.children
+      .filter(item => !!item)
+      .map(item => typeFactory(item));
+
+    if (children.length > 0) {
+      return children;
+    }
+
+    return this.getEmptyContent();
+  }
+
+  getEmptyContent() {
+    return (
+      <div
+        className={`card-body-empty-content ${s['card-body-empty-content']}`}
+      >
+        <img src={emptyImg} />
       </div>
     );
   }
